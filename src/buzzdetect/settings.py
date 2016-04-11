0582acd60d5d3f3afa99cb8ccb38e0f8c20cede9
jpic <jamespic@gmail.com>
2016-04-11 21:55:39 +0000

    Server fixes

--- src/buzzdetect/settings.py
+++ src/buzzdetect/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^6^e97q8m&n$240#3tk@r1y3se$shpw@oy67qgfo6v)i4mshpu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,4 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/collected/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected')
