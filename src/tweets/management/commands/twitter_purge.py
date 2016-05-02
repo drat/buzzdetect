8cfc51e03e485c76551211ddfc54ef286fcab5e8
jpic <jamespic@gmail.com>
2016-05-02 11:36:14 +0200

    Update twitter_purge

--- src/tweets/management/commands/twitter_purge.py
+++ src/tweets/management/commands/twitter_purge.py
@@ -4,27 +4,28 @@ from datetime import datetime, timedelta
 
 from django.core.management.base import BaseCommand
 
-from tweets.models import Tweet, Retweets
-from tweets.utils import get_data
+from posts.models import Post, Poster
 
 import pytz
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        min_datetime = (
-            datetime.now(tz=pytz.utc)
-            - timedelta(hours=
-                os.environ.get(
-                    'DELETE_TWEETS_WITH_MORE_HOURS_THAN'
-                )
+        for poster in Poster.objects.all():
+            recent_posts = poster.post_set.order_by('-datetime')
+            best_posts = poster.post_set.order_by('-last_stat__reposts')
+
+            posts = poster.post_set.exclude(
+                pk__in=recent_posts.values_list('pk', flat=True)[:50]
+            ).exclude(
+                pk__in=best_posts.values_list('pk', flat=True)[:50]
             )
-        )
 
-        old = Tweet.objects.filter(
-            datetime__lte=min_datetime,
-        )
+            print 'Would delete %s out of %s posts by %s' % (
+                posts.count(),
+                poster.post_set.count(),
+                poster,
+            )
 
-        print 'Deleting %s old tweets' % old.count()
-
-        old.delete()
+            if posts.count():
+                posts.delete()
