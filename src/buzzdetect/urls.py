94791791bd95cd965174b54d0648ad0879a80436
jpic <jamespic@gmail.com>
2016-10-12 11:43:36 +0200

    Require authentication for index

--- src/buzzdetect/urls.py
+++ src/buzzdetect/urls.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views import generic
 
 urlpatterns = [
-    url(r'^$', generic.TemplateView.as_view(template_name='index.html')),
+    url(
+        r'^$',
+        login_required(
+            generic.TemplateView.as_view(template_name='index.html'),
+        )
+    ),
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls', namespace='posts')),
 ]
